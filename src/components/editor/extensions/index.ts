826ac57e8f64d412636b2a5549f3859dd4475d2c
Snehal Dangroshiya <snehaldangroshiya@gmail.com>
2026-04-17 15:23:33 +0530

    feat(editor): barrel export for CM6 extensions

---
+++ src/components/editor/extensions/index.ts
@@ -0,0 +1,5 @@
+export { singleLineFilter } from './single-line-filter';
+export { rocketTheme, rocketThemeDark } from './theme';
+export { variableAutocomplete, variableCompletionSource } from './variable-autocomplete';
+export { setVariableContextEffect, variableContextFacet } from './variable-context-facet';
+export { variableHighlight } from './variable-highlight';
